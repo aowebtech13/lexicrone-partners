76c919375ae4eab56389a7a9a99bd2f8f78b0214
Omooba442 <adesokanadedeji10@gmail.com>
2026-02-08 06:30:16 +0100

    streamlining files

---
+++ frontend/src/components/register/index.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import RegisterArea from "./register-area";
+
+const Register = () => {
+  return (
+    <>
+      
+      <RegisterArea />
+    </>
+  );
+};
+
+export default Register;
